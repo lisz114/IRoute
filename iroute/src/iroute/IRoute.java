fe618f1370b71e4e0fea78bde2b31ad84938f300
lisz114 <vitoriaelisa114@gmail.com>
2023-09-20 21:15:51 -0300

    Modificação do Estudo de caso
    método menu, login e cadastro

--- iroute/src/iroute/IRoute.java
+++ iroute/src/iroute/IRoute.java
@@ -1,60 +1,79 @@
 package iroute;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class IRoute {
 
-	
 	public static void main(String[] args) {
 
-		Integer opcaoSelecionada = Integer.MAX_VALUE;
+		Scanner leitura = new Scanner(System.in);
+
+		Integer opcaoSelecionada = menu();
+		switch (opcaoSelecionada) {
+		case 1: {
+			cadastro();
+		}
+			break;
+		case 2: {
+			login();
+		}
+			break;
+		default: {
+			System.out.println("Erro! Escolha uma das alternativas.");
+			main(args);
+		}
+		}
+	}
+
+	private static Integer menu() {
+		Scanner leitura = new Scanner(System.in);
+
+		System.out.println("- MENU -");
+		System.out.println("1 CADASTRAR");
+		System.out.println("2 LOGIN");
+
+		Integer opcaoSelecionada = Integer.valueOf(leitura.nextLine());
+
+		return opcaoSelecionada;
+
+	}
+
+	private static void cadastro() {
 		Scanner leitura = new Scanner(System.in);
 		ArrayList<NovoUsuario> NovoUsuarios = new ArrayList<>();
 
-		while (opcaoSelecionada != 0) {
-			System.out.println("- MENU -");
-			System.out.println("1 CADASTRAR");
-			System.out.println("2 LOGIN");
+		System.out.println("Digite o número do seu Cartão: ");
+		Integer cartao = Integer.valueOf(leitura.nextLine());
+		System.out.println("Digite seu nome inteiro: ");
+		String nome = leitura.nextLine();
+		System.out.println("Crie uma senha: ");
+		String senha = leitura.nextLine();
 
-			opcaoSelecionada = Integer.valueOf(leitura.nextLine());
+		NovoUsuario p = new NovoUsuario();
+		p.setNumeroCartao(cartao);
+		p.setNome(nome);
+		p.setSenha(senha);
+		NovoUsuarios.add(p);
 
-			switch (opcaoSelecionada) {
-			case 1: {
-				System.out.println("Digite o número do seu Cartão: ");
-				Float cartao = Float.valueOf(leitura.nextLine());
-				
-				//ACABAMOS AQUI
+		leitura.close();
 
-				NovoUsuario p = new NovoUsuario();
-				p.setNome(nome);
-				p.setCpf(Long.valueOf(cpf));
+	}
 
-				NovoUsuarios.add(p);
+	private static void login() {
 
-			}
-			case 2: {
-				System.out.println("Digite o cpf da NovoUsuario que deseja alterar:");
-				// finalizar codigo
-			}
-			case 3: {
-				System.out.println("Digite o cpf da NovoUsuario que deseja excluir:");
-				// finalizar codigo
-			}
-			case 4: {
-				// Saida de dados (output)
-				System.out.println("Listagem de NovoUsuarios cadastradas: ");
-				for (NovoUsuario NovoUsuario : NovoUsuarios) {
-					System.out.println("Cpf: " + NovoUsuario.getCpf());
-					System.out.println("Nome: " + NovoUsuario.getNome());
-				}
-			}
+		Scanner leitura = new Scanner(System.in);
+		ArrayList<NovoUsuario> NovoUsuarios = new ArrayList<>();
+		System.out.println("Digite o seu nome de usuário");
+		String nome = leitura.nextLine();
+		System.out.println("Digite a senha: ");
+		String senha = leitura.nextLine();
 
-			}
-
-		}
+		NovoUsuario p = new NovoUsuario();
+		p.setNome(nome);
+		p.setSenha(senha);
+		NovoUsuarios.add(p);
 
 		leitura.close();
 	}
-}
-
 }
--- iroute/src/iroute/NovoUsuario.java
+++ iroute/src/iroute/NovoUsuario.java
@@ -2,25 +2,28 @@ package iroute;
 
 public class NovoUsuario {
 
+
 	private String nome;
-	private String senha;
-	private Float numeroCartao;
 	public String getNome() {
 		return nome;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
+	private String senha;
 	public String getSenha() {
 		return senha;
 	}
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
-	public Float getNumeroCartao() {
+	
+	private Integer numeroCartao;
+	public Integer getNumeroCartao() {
 		return numeroCartao;
 	}
-	public void setNumeroCartao(Float numeroCartao) {
+	public void setNumeroCartao(Integer numeroCartao) {
 		this.numeroCartao = numeroCartao;
 	}
 }
